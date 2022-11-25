3b5a9b53ffcfba7afb5b04fc09fce27f85e8b73b
Laura Jürgensmeier <46404911+laura-jmr@users.noreply.github.com>
2022-11-25 21:03:45 +0100

    JS: admina and guest login

---
+++ index.js
@@ -0,0 +1,19 @@
+const login_data = {admin: {name: 'admina', password: "password"},
+                    guest: {name: 'guest', password: "password"}}
+
+function check_login(){
+    const form = document.getElementById("login");
+    const form_data = new FormData(form);
+    //console.log(form_data.get("password"), form_data.get("username"));
+    const users = Object.values(login_data);
+
+    for (let i = 0; i < users.length; i++){
+        if (form_data.get("username") == users[i].name){
+            if (form_data.get("password") == users[i].password){
+                return users[i].name;
+            };
+            return "false password";
+        };
+    };
+    return null;
+}
